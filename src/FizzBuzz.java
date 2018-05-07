1cf54d6a5e25a1a6fe0a45c0c3dee0cecbf8800d
Aaron136 <171wita@o365.htl-leoben.at>
2018-05-07 15:05:09 +0200

    Temperatur, Version 1

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,21 +2,28 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
-			if (i % 3 == 0 && i % 5 == 0) {
-				System.out.print("FizzBuzz ");
-				System.out.println("zzzzz");
-			} else if (i % 3 == 0) {
-				System.out.print("Fizz");
-			} else if (i % 5 == 0) {
-				System.out.print("Buzz");
-			} else {
-				System.out.print(i);
-
-			}
-			System.out.println();
+//		for (int i = 1; i <= 100; i++) {
+//			FizzerBuzzer(i); // bei 4: fizzer, bei 7: buzzer
+//		}
+//		
+		for (int n = 0; n <= 100; n+=4) {
+			FizzerBuzzer(n, false);
 		}
 		System.out.println("ende!");
+	}
+	
+	public static void FizzerBuzzer(int i, boolean showFizzBuzz) {
+		if (i % 4 == 0 && i % 7 == 0 && showFizzBuzz == true) {
+			System.out.print("FizzerBuzzer ");
+		} else if (i % 4 == 0) {
+			System.out.print("Fizzer ("+i+")");
+		} else if (i % 7 == 0) {
+			System.out.print("Buzzer");
+		} else {
+			System.out.print(i);
+
+		}
+		System.out.println();
 	}
 
 }
